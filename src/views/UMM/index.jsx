98b05a55ee0f836faf00758edd5ccaacecc79240
Dany Fortin-Simard <dany@botpress.io>
2017-06-07 23:14:40 -0400

    Create a view for quick replies and typing

--- src/views/UMM/index.jsx
+++ src/views/UMM/index.jsx
@@ -1,22 +1,95 @@
 import { Component } from 'react'
+import { 
+  Button,
+  Tooltip,
+  OverlayTrigger
+} from 'react-bootstrap'
+
 import classnames from 'classnames'
+import Spinner from 'react-spinkit'
 
 import style from './style.scss'
 
 export default class UMMComponent extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      typing: false
+    }
   }
 
-  renderText() {
-    const classNames = classnames(style.text, 'bp-messenger-text')
-    return <div className={classNames}>
-        {this.props.text}
-      </div>
+  componentDidMount() {
+    this.setTyping()
+  }
+
+  componentWillReceiveProps() {
+    this.setTyping()
+  }
+
+  setTyping() {
+    if (this.props.raw.typing) {
+      this.setState({
+        typing: true
+      })
+    }
+
+    setTimeout(() => {
+      this.setState({
+        typing: false
+      })
+    }, this.props.raw.typing)
   }
 
   renderTyping() {
-    return <div>Typing</div> 
+    const classNames = classnames({
+      [style.typing]: this.state.typing,
+      'bp-messenger-typing': this.state.typing
+    })
+
+    return <div className={classNames}>
+      <Spinner name='ball-beat' fadeIn={'quarter'} className={style.spinner}/>
+    </div>
+  }
+
+  renderText() {
+    const classNames = classnames({
+      [style.text]: true, 
+      'bp-messenger-text': true
+    })
+    
+    if (this.state.typing) {
+      return this.renderTyping()
+    }
+
+    return <div>
+        <div className={classNames}>
+          {this.props.text}
+        </div>
+        {this.renderQuickReplies()}
+      </div>
+  }
+
+  renderButton({ title, payload }, key) {
+    const tooltip = <Tooltip id="tooltip">
+      On click, payload event <strong>{payload}</strong> is emitted.
+    </Tooltip>
+
+    return <OverlayTrigger key={key} placement="top" overlay={tooltip}>
+      <Button >{title}</Button>
+    </OverlayTrigger>
+  }
+
+  renderQuickReplies() {
+    if (!this.props.raw.quick_replies) {
+      return null
+    }
+
+    const classNames = classnames(style.quickReplies, 'bp-messenger-quick-replies')
+    
+    return <div className={classNames}>
+        {this.props.raw.quick_replies.map(this.renderButton)}
+      </div>
   }
 
   renderNotSupported() {
@@ -27,14 +100,13 @@ export default class UMMComponent extends Component {
     switch (this.props.type) {
     case 'text':
       return this.renderText()
-    case 'typing':
-      return this.renderTyping()
     default:
       return this.renderNotSupported()
     }   
   }
 
   render() {
+    console.log(this.props)
     const classNames = classnames(style.component, 'bp-messenger-component')
     return <div className={classNames}>
         {this.renderComponent()}
