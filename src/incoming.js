6ce145f0ba3ca4d922c3fdffa33381aa64df23f6
Phil <phillip.havea@gmail.com>
2017-08-25 16:04:52 +1000

    added account linking

--- src/incoming.js
+++ src/incoming.js
@@ -160,7 +160,15 @@ module.exports = (bp, messenger) => {
   })
 
   messenger.on('account_linking', () => {
-    bp.logger.warn('[messenger] ACCOUNT_LINKING NOT IMPLEMENTED, Your pull requests are welcome :)')
+      preprocessEvent(e).then(function (profile) {
+          bp.middlewares.sendIncoming({
+              platform: 'facebook',
+              type: 'account_linking',
+              user: profile,
+              text: e.account_linking.authorization_code,
+              raw: e
+          });
+      });
   })
 
   messenger.on('optin', e => {
